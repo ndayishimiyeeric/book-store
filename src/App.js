b54097f73b4eb9ea3e88efc9bd958cd809aa8ea2
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-06-03 00:57:29 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Books from './components/Books';
+import Categories from './components/Categories';
+
 function App() {
   return (
-    <div className="App">
-      Helo from book store
+    <div className="container">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }

e78ce25b5bce4c1c9ad6071a1313be040f39a68c
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-06-08 00:05:52 +0200

    Consume book state

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
 
 function Books() {
+  const books = useSelector((state) => state.bookReducer);
+  const booksElement = books.map((book) => (
+    <Book
+      key={book.id}
+      id={book.id}
+      title={book.title}
+      author={book.author}
+    />
+  ));
+
   return (
     <div>
-      <Book title="Book 1" />
-      <Book title="Book 2" />
+      {books.length !== 0 && booksElement}
+      {books.length === 0 && 'No Books Available'}
       <Form />
     </div>
   );

8b983b7a089f9fc804a8c1fa6fb0eaea7e8e6c86
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-06-09 22:32:53 +0200

    Style book

--- src/components/Book.js
+++ src/components/Book.js
@@ -17,10 +17,36 @@ function Book(props) {
   };
   return (
     <div className={styles.book}>
-      <p>{title}</p>
-      <p>{ author }</p>
-      <p>{ category }</p>
-      <Button id={id} name="Remove" handleClick={handleClick} />
+      <div className={styles.details}>
+        <p className={styles.detailsCategory}>{ category }</p>
+        <p className={styles.detailsTitle}>{title}</p>
+        <p className={styles.detailsAuthor}>{author}</p>
+        <div className={styles.detailsButtons}>
+          <Button id={id} name="Comments" />
+          <div className={styles.smline}>{ }</div>
+          <Button id={id} name="Remove" handleClick={handleClick} />
+          <div className={styles.smline}>{ }</div>
+          <Button id={id} name="Edit" />
+        </div>
+      </div>
+      <div className={styles.progress}>
+        <div className={styles.progressCircle}>
+          <svg>
+            <circle cx="40" cy="40" r="40">{ }</circle>
+            <circle cx="40" cy="40" r="40">{ }</circle>
+          </svg>
+        </div>
+        <div className={styles.progressParcent}>
+          <p className={styles.progressNumber}>65%</p>
+          <p className={styles.progressCompleted}>Completed</p>
+        </div>
+      </div>
+      <div className={styles.lgline}>{ }</div>
+      <div className={styles.chapters}>
+        <p className={styles.chaptersTitle}>CURRENT CHAPTER</p>
+        <p className={styles.chaptersChap}>Chapter 2</p>
+        <button className={styles.button} type="button">UPDATE PROGRESS</button>
+      </div>
     </div>
   );
 }

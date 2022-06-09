53711f4825d293477d56ec3e278088c50d9b8851
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-06-09 12:51:14 +0200

    create asyncThunks

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,42 +1,77 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/8zgJXl3G9vojkee6XZ5H/books';
 
 // INITIAL STATE
 const initialState = {
-  books: [
-    {
-      id: 1,
-      title: 'Lean Redux',
-      author: 'Microverse',
-    },
-    {
-      id: 2,
-      title: 'Lean Advanced Redux',
-      author: 'FreeCodeCamp',
-    },
-    {
-      id: 3,
-      title: 'Learn ReduxToolkit',
-      author: 'Code Cademy',
-    },
-  ],
+  books: [],
+  isLoading: false,
 };
+
+export const getBooks = createAsyncThunk('books/getbooks', async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  const books = [Object.keys(data).map((key) => (
+    {
+      id: key,
+      ...data[key][0],
+    }
+  ))];
+  return books;
+});
+
+export const addBook = createAsyncThunk(
+  'books/addBook', async (payload, thunkAPI) => {
+    await fetch(`${url}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: payload.id,
+        title: payload.title,
+        author: payload.author,
+        category: payload.category,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(() => thunkAPI.dispatch(getBooks()));
+    const { books } = thunkAPI.getState().books;
+    return books;
+  },
+);
+
+export const removeBook = createAsyncThunk(
+  'books/removeBook', async (payload, thunkAPI) => {
+    await fetch(`${url}/${payload.id}`, {
+      method: 'DELETE',
+    }).then(() => thunkAPI.dispatch(getBooks()));
+    const { books } = thunkAPI.getState().books;
+    return books;
+  },
+);
 
 const bookSlice = createSlice({
   name: 'book',
   initialState,
-  reducers: {
-    addBook: (state, { payload }) => {
-      state.books = [ //eslint-disable-line
-        ...state.books,
-        payload,
-      ];
+  reducers: {},
+  extraReducers: {
+    [getBooks.pending]: (state) => {
+      state.isLoading = true;
     },
-    removeBook: (state, { payload }) => {
-      state.books = state.books.filter((item) => item.id !== payload.id); //eslint-disable-line
+    [getBooks.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.books = action.payload[0]; //eslint-disable-line
+    },
+    [getBooks.rejected]: (state) => {
+      state.isLoading = false;
+    },
+    [addBook.fulfilled]: (state, action) => {
+      state.books = action.payload;
+    },
+    [removeBook.fulfilled]: (state, action) => {
+      state.books = action.payload;
     },
   },
 });
-
-export const { addBook, removeBook } = bookSlice.actions;
 
 export default bookSlice.reducer;

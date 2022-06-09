7de707ca184ebc5790a8a1661bd5bebbdbcf1386
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-06-09 18:53:34 +0200

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { VscAccount } from 'react-icons/vsc';
+import { ImUser } from 'react-icons/im';
 import styles from './style/Navbar.module.css';
 
 function Navbar() {
+  const [color, setColor] = React.useState({
+    books: true,
+    category: false,
+  });
+  const style = {
+    opacity: 1,
+  };
+
+  const hanleBooks = () => {
+    setColor({
+      books: true,
+      category: false,
+    });
+  };
+
+  const hanleCategory = () => {
+    setColor({
+      books: false,
+      category: true,
+    });
+  };
   return (
-    <header>
-      <h2 className="logo">BookStore CMS</h2>
+    <header className={styles.header}>
+      <p className={styles.logo}>Bookstore CMS</p>
       <ul className={styles.links}>
-        <li><Link to="/">Books</Link></li>
-        <li><Link to="/categories">Categories</Link></li>
+        <li>
+          <Link
+            className={styles.link}
+            to="/"
+            style={color.books ? style : null}
+            onClick={hanleBooks}
+          >
+            BOOKS
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={styles.link}
+            to="/categories"
+            style={color.category ? style : null}
+            onClick={hanleCategory}
+          >
+            CATEGORIES
+          </Link>
+        </li>
       </ul>
-      <VscAccount className={styles.account} />
+      <div className={styles.userCircle}>
+        <ImUser className={styles.account} />
+      </div>
     </header>
   );
 }
